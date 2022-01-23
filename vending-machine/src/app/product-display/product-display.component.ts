944cc83372afd1bf36ef03a968a1fdf7ef9d3864
@n0nym0u5 <master@pop-os.localdomain>
2022-01-24 01:56:47 +0545

    To-Do: Handle state using ngModel

--- vending-machine/src/app/product-display/product-display.component.ts
+++ vending-machine/src/app/product-display/product-display.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { empty } from 'rxjs';
 import { ProductService } from 'src/services/product.service';
 
 interface Product {
@@ -17,6 +18,7 @@ interface Product {
 export class ProductDisplayComponent implements OnInit {
   allProducts: any;
   imgSource: any;
+  greaterThanStock: any;
   constructor(private service: ProductService) { 
     this.imgSource = '/assets/images/coke.jpg'
   }
@@ -28,8 +30,26 @@ export class ProductDisplayComponent implements OnInit {
     });
   }
 
-  quantity(productName: string, quantity: any) {
-    console.log(productName, quantity);
+  quantity(product: any, quantity: any) {
+    console.log(<number>(quantity.target.value))
+    console.log(quantity.target.value);
+    let initialStock = product.stock;
+    let stockAfterQuantity = initialStock - <number>quantity.target.value;
+
+    console.log(stockAfterQuantity);
+
+    if(stockAfterQuantity < 0 ) {
+      product.stock = 10;
+      this.greaterThanStock = true
+    }
+    if(quantity.target.value === "") {
+      product.stock = initialStock
+    }
+    
+    if(quantity.target.value <= initialStock) {
+      product.stock = stockAfterQuantity;
+    } 
+    console.log(product)
   }
 
 }
