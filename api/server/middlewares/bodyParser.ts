c30d59086387866814443d15e83dba3e5747def1
@n0nym0u5 <master@pop-os.localdomain>
2022-01-24 00:12:49 +0545

    Vending Machine Database

--- api/server/middlewares/bodyParser.ts
+++
@@ -1,9 +0,0 @@
-import bodyParser from 'body-parser';
-import defineExports from '../majorExports/exportExpressRelated';
-
-let bodyParserInjection = () => {
-    defineExports().APP.use(bodyParser.json());
-    defineExports().APP.use(bodyParser.urlencoded({ extended: false })
-    );
-}
-export default bodyParserInjection;
